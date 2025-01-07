47b1d44f1d25b710f0661e123292081e117b1aeb
Joshua Riefman <joshuariefman@gmail.com>
2025-01-07 01:01:24 -0800

    Fix distribution in build process

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, Extension, Distribution
 from setuptools.command.build_py import build_py
 import subprocess
 import os
@@ -32,11 +32,50 @@ class GoBuildExt(build_py):
 
         super().run()
 
+#  setuptools has no idea that we are building extensions manually and putting them into the build directory, and
+#  so it thinks the resulting wheel is pure Python, which causes cibuildwheel to fail. To fix this, we need to
+#  override a few things in how the distribution metadata is constructed,
+#  https://stackoverflow.com/questions/76450587/python-wheel-that-includes-shared-library-is-built-as-pure-python-platform-indep
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class MyWheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            python, abi = 'py3', 'none'
+            return python, abi, plat
+
+    class MyDistribution(Distribution):
+
+        def __init__(self, *attrs):
+            Distribution.__init__(self, *attrs)
+            self.cmdclass['bdist_wheel'] = MyWheel
+
+        def is_pure(self):
+            return False
+
+        def has_ext_modules(self):
+            return True
+
+except ImportError:
+    class MyDistribution(Distribution):
+        def is_pure(self):
+            return False
+
+        def has_ext_modules(self):
+            return True
+
 
 setup(
     cmdclass={"build_py": GoBuildExt},
+    distclass=MyDistribution,
     packages=["fast_perlin_noise"],
     package_data={
-        "fast_perlin_noise": ["../src/*"],  # Include the `src/` folder
+        "fast_perlin_noise": ["build/lib/fast_perlin_noise/*.so", "build/lib/fast_perlin_noise/*.h"],  # Ensure .so and .h are included
     },
+    include_package_data=True,  # Ensure package_data is honored
 )
